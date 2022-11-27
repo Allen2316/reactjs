c8e94aa45fbca418dcf8d0023c65836984577959
Luis Alberto Sarmiento Cuenca <52260850+Allen2316@users.noreply.github.com>
2022-11-27 15:43:30 -0500

    Actualizacion de tic tac toe

--- src/index.js
+++ src/index.js
@@ -2,37 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {  
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}>
-                {this.props.value}
-            </button>
-        );
-    }
+function square(props) {
+    return (
+        <button
+            className="square"
+            onClick={props.onClick()}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state ={
-            squares : Array(9).fill(null),
+        this.state = {
+            squares: Array(9).fill(null),
         };
     }
 
-    handleClick(i){
+    handleClick(i) {
         const squares = this.state.squares.slice();
         squares[i] = 'x';
-        this.setState({squares:squares});
+        this.setState({ squares: squares });
     }
 
     renderSquare(i) {
         return (
-            <Square 
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)}
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
             />
         );
     }
